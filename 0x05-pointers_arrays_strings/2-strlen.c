c0f1de7405938a4fd9aa055d160512185052623b
ayo <ayo@pop-os.localdomain>
2023-10-16 20:24:43 +0100

    Fix 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen - counts the length of a string
- * @s: the adress of a string
- * return :  returns the length of a string
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
  */
 int _strlen(char *s)
 {
 int count = 0;
 
-while (*s != '\0')
-{
+while (*(s + count) != '\0')
 count++;
-}
 return (count);
 }
