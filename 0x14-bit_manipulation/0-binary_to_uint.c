5cbfc5d516eea2f2d2d3997f0b27f6103cc26560
ayo <ayo@pop-os.localdomain>
2023-11-23 21:19:25 +0100

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,40 +7,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int str_length = 0, base = 1;
+unsigned int result = 0;
 
-if (!string_check(b))
-return (0);
-
-while (b[str_length] != '\0')
-str_length++;
-
-while (str_length)
-{
-decimal += ((b[str_length - 1] - '0') * base);
-base *= 2;
-str_length--;
-}
-return (decimal);
-}
-
-/**
- * string_check - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int string_check(const char *b)
-{
 if (b == NULL)
 return (0);
 
 while (*b)
 {
-if (*b != '1' && *b != '0')
-return (0);
+if (*b == '0' || *b == '1')
+{
+result = result * 2 + (*b - '0');
 b++;
 }
-return (1);
+else
+{
+return (0);
+}
+}
+
+return (result);
 }
